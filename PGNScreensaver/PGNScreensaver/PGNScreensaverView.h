3310c2a685347f733b24e0f5ff8230bab1c4b1b2
Bogdan-Constantin Popescu <bogdan.popescu0905@gmail.com>
2018-02-23 13:14:10 +0100

    Add workspace and project file

---
+++ PGNScreensaver/PGNScreensaver/PGNScreensaverView.h
@@ -0,0 +1,13 @@
+//
+//  PGNScreensaverView.h
+//  PGNScreensaver
+//
+//  Created by Bogdan Constantin Popescu on 23/02/2018.
+//  Copyright © 2018 MathPlayer. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface PGNScreensaverView : ScreenSaverView
+
+@end
