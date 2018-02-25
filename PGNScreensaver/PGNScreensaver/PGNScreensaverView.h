ac2aa3c30ce351318ebe0f807f646e9b4ed660f5
Bogdan-Constantin Popescu <bogdan.popescu0905@gmail.com>
2018-02-25 15:36:43 +0100

    Load initial position

--- PGNScreensaver/PGNScreensaver/PGNScreensaverView.h
+++ PGNScreensaver/PGNScreensaver/PGNScreensaverView.h
@@ -9,9 +9,31 @@
 #import <ScreenSaver/ScreenSaver.h>
 
 @interface PGNScreensaverView : ScreenSaverView {
+    char position[8][8];
+
+    NSImage *whiteKing;
+    NSImage *blackKing;
+
+    NSImage *whiteQueen;
+    NSImage *blackQueen;
+
+    NSImage *whiteBishop;
+    NSImage *blackBishop;
+
+    NSImage *whiteKnight;
+    NSImage *blackKnight;
+
+    NSImage *whiteRook;
+    NSImage *blackRook;
+
+    NSImage *whitePawn;
+    NSImage *blackPawn;
+
     BOOL drawChessBoard;
 }
 
 - (void)drawChessBoard;
+- (void)setInitialPosition;
+- (void)loadImages;
 
 @end
--- PGNScreensaver/PGNScreensaver/PGNScreensaverView.m
+++ PGNScreensaver/PGNScreensaver/PGNScreensaverView.m
@@ -18,6 +18,8 @@
     if (self) {
         [self setAnimationTimeInterval:1/30.0];
         drawChessBoard = YES;
+        [self setInitialPosition];
+        [self loadImages];
     }
     return self;
 }
@@ -58,18 +60,58 @@
                                      self.frame.size.height / 2 - minDimension / 2);
     CGFloat sqSize = minDimension / 8;
 
-    for (int i = 0; i < 8; ++i) {
-        for (int j = 0; j < 8; ++j) {
-            NSRect r = NSMakeRect(bottomLeft.x + i * sqSize,
-                                  bottomLeft.y + j * sqSize,
+    for (int y = 0; y < 8; ++y) {
+        for (int x = 0; x < 8; ++x) {
+            NSRect r = NSMakeRect(bottomLeft.x + x * sqSize,
+                                  bottomLeft.y + y * sqSize,
                                   sqSize,
                                   sqSize);
-            if ((i + j) % 2) {
+            if ((x + y) % 2) {
                 [NSColor.chessBoardBrownDarkColor set];
             } else {
                 [NSColor.chessBoardBrownLightColor set];
             }
             NSRectFill(r);
+            switch (position[y][x]) {
+                case 'K':
+                    [whiteKing drawInRect:r];
+                    break;
+                case 'k':
+                    [blackKing drawInRect:r];
+                    break;
+                case 'Q':
+                    [whiteQueen drawInRect:r];
+                    break;
+                case 'q':
+                    [blackQueen drawInRect:r];
+                    break;
+                case 'R':
+                    [whiteRook drawInRect:r];
+                    break;
+                case 'r':
+                    [blackRook drawInRect:r];
+                    break;
+                case 'B':
+                    [whiteBishop drawInRect:r];
+                    break;
+                case 'b':
+                    [blackBishop drawInRect:r];
+                    break;
+                case 'N':
+                    [whiteKnight drawInRect:r];
+                    break;
+                case 'n':
+                    [blackKnight drawInRect:r];
+                    break;
+                case 'P':
+                    [whitePawn drawInRect:r];
+                    break;
+                case 'p':
+                    [blackPawn drawInRect:r];
+                    break;
+                default:
+                    break;
+            }
         }
     }
 
@@ -84,6 +126,40 @@
 - (NSWindow*)configureSheet
 {
     return nil;
+}
+
+- (void)setInitialPosition
+{
+    strcpy(position[7], "rnbqkbnr");
+    strcpy(position[6], "pppppppp");
+    strcpy(position[5], "        ");
+    strcpy(position[4], "        ");
+    strcpy(position[3], "        ");
+    strcpy(position[2], "        ");
+    strcpy(position[1], "PPPPPPPP");
+    strcpy(position[0], "RNBQKBNR");
+}
+
+- (void)loadImages
+{
+    NSBundle *b = [NSBundle bundleForClass:[self class]];
+    whiteKing = [[NSImage alloc] initWithContentsOfFile:[b pathForResource:@"Chess_klt45" ofType:@"pdf"]];
+    blackKing = [[NSImage alloc] initWithContentsOfFile:[b pathForResource:@"Chess_kdt45" ofType:@"pdf"]];
+
+    whiteQueen = [[NSImage alloc] initWithContentsOfFile:[b pathForResource:@"Chess_qlt45" ofType:@"pdf"]];
+    blackQueen = [[NSImage alloc] initWithContentsOfFile:[b pathForResource:@"Chess_qdt45" ofType:@"pdf"]];
+
+    whiteRook = [[NSImage alloc] initWithContentsOfFile:[b pathForResource:@"Chess_rlt45" ofType:@"pdf"]];
+    blackRook = [[NSImage alloc] initWithContentsOfFile:[b pathForResource:@"Chess_rdt45" ofType:@"pdf"]];
+
+    whiteBishop = [[NSImage alloc] initWithContentsOfFile:[b pathForResource:@"Chess_blt45" ofType:@"pdf"]];
+    blackBishop = [[NSImage alloc] initWithContentsOfFile:[b pathForResource:@"Chess_bdt45" ofType:@"pdf"]];
+
+    whiteKnight = [[NSImage alloc] initWithContentsOfFile:[b pathForResource:@"Chess_nlt45" ofType:@"pdf"]];
+    blackKnight = [[NSImage alloc] initWithContentsOfFile:[b pathForResource:@"Chess_ndt45" ofType:@"pdf"]];
+
+    whitePawn = [[NSImage alloc] initWithContentsOfFile:[b pathForResource:@"Chess_plt45" ofType:@"pdf"]];
+    blackPawn = [[NSImage alloc] initWithContentsOfFile:[b pathForResource:@"Chess_pdt45" ofType:@"pdf"]];
 }
 
 @end
