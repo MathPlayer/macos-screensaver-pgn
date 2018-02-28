4abbcb44c04c1a4b8aff97b3d4784e0382404ec3
Bogdan-Constantin Popescu <bogdan.popescu0905@gmail.com>
2018-02-28 17:59:50 +0100

    Extract chess common definitions

--- PGNScreensaver/PGNScreensaver/ChessBoard.h
+++ PGNScreensaver/PGNScreensaver/ChessBoard.h
@@ -8,37 +8,10 @@
 
 #import <Foundation/Foundation.h>
 
-typedef enum : NSUInteger {
-    WHITE = 0,
-    BLACK = 1
-} ChessPieceColor;
-
-typedef enum : NSUInteger {
-    NONE = 0,
-    PAWN = 2,
-    KNIGHT = 4,
-    BISHOP = 8,
-    ROOK = 16,
-    QUEEN = 32,
-    KING = 64,
-
-    WHITE_PAWN = WHITE + PAWN,
-    WHITE_KNIGHT = WHITE + KNIGHT,
-    WHITE_BISHOP = WHITE + BISHOP,
-    WHITE_ROOK = WHITE + ROOK,
-    WHITE_QUEEN = WHITE + QUEEN,
-    WHITE_KING = WHITE + KING,
-
-    BLACK_PAWN = BLACK + PAWN,
-    BLACK_KNIGHT = BLACK + KNIGHT,
-    BLACK_BISHOP = BLACK + BISHOP,
-    BLACK_ROOK = BLACK + ROOK,
-    BLACK_QUEEN = BLACK + QUEEN,
-    BLACK_KING = BLACK + KING
-} ChessPieceType;
+#import "ChessCommonDefines.h"
 
 @interface ChessBoard : NSObject {
-    NSUInteger _board[8][8];
+    ChessPieceType _board[8][8];
 
     // Use values from ChessPieceType enum as keys
     NSDictionary<NSNumber *, NSImage *> *_images;
