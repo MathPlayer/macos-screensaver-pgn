c1eaf9a6f47e06eadc0f1db282a0b315cb123894
Bogdan-Constantin Popescu <bogdan.popescu0905@gmail.com>
2018-05-18 08:47:11 +0200

    GameManager: add rook move when castling

--- PGNScreensaver/PGNScreensaver/GameManager.m
+++ PGNScreensaver/PGNScreensaver/GameManager.m
@@ -10,6 +10,16 @@
 
 @implementation GameManager
 
+
+static NSDictionary *_castlingMoves;
+
++ (void)initialize {
+    _castlingMoves = @{@"e1c1": @"a1d1",
+                       @"e8c8": @"a8d8",
+                       @"e1g1": @"h1f1",
+                       @"e8g8": @"h8f8"};
+}
+
 + (instancetype)managerWithFile:(NSString *)filename
 {
     return [[self alloc] initWithFile:filename];
@@ -23,12 +33,16 @@
         // TODO: load a PGN file from filename and parse a similar content to this hardcoded one
         // Content extracted from output of command:
         // ./pgn-extract ../macos-screensaver-pgn/DefaultGame.pgn
-        //     -Wlalg -V -C -N --nochecks -w9999 -7 --nomovenumbers --noresults
-        // NSString *hardcodedMoves = @"e2e4 e7e5 g1f3 d7d6 d2d4 c8g4 d4e5 g4f3 d1f3 d6e5 f1c4 g8f6 f3b3 d8e7 b1c3 c7c6 c1g5 b7b5 c3b5 c6b5 c4b5 b8d7 e1c1 a8d8 d1d7 d8d7 h1d1 e7e6 b5d7 f6d7 b3b8 d7b8 d1d8";
-        NSString *hardcodedMoves = @"e2e4 c7c6 d2d4 d7d5 b1c3 d5e4 c3e4 b8d7 d1e2 g8f6 e4d6";
+        //     -Welalg -V -C -N --nochecks -w9999 -7 --nomovenumbers --noresults
+        NSString *hardcodedMoves = @"e2e4 e7e5 Ng1f3 d7d6 d2d4 Bc8g4 d4e5 Bg4f3 Qd1f3 d6e5 Bf1c4 Ng8f6 Qf3b3 Qd8e7 Nb1c3 c7c6 Bc1g5 b7b5 Nc3b5 c6b5 Bc4b5 Nb8d7 e1c1 Ra8d8 Rd1d7 Rd8d7 Rh1d1 Qe7e6 Bb5d7 Nf6d7 Qb3b8 Nd7b8 Rd1d8";
+        // NSString *hardcodedMoves = @"e2e4 c7c6 d2d4 d7d5 Nb1c3 d5e4 c3e4 Nb8d7 Qd1e2 Ng8f6 Ne4d6";
         NSMutableArray<Move *> *readMoves = [NSMutableArray array];
         for (NSString *moveString in [hardcodedMoves componentsSeparatedByCharactersInSet:[NSCharacterSet whitespaceCharacterSet]]) {
-            [readMoves addObject:[Move moveWithString:moveString]];
+            [readMoves addObject:[Move moveFromString:moveString]];
+            NSString *extraMove = _castlingMoves[moveString];
+            if (extraMove) {
+                [readMoves addObject:[Move moveFromString:extraMove]];
+            }
         }
         _moves = [NSArray arrayWithArray:readMoves];
     }
